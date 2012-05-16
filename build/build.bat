c3faff5754c08332e1ec43085a00b1bb1f855158
Alasdair North <alnorth@gmail.com>
2012-05-16 12:01:04 +0100

    Add pretty printed version of compiled script for debugging.

--- build/build.bat
+++ build/build.bat
@@ -1,1 +1,2 @@
 java -jar compiler.jar --js ../quolace.js --js_output_file quolace.min.js --compilation_level ADVANCED_OPTIMIZATIONS --externs externs/jquery-1.7.js
+java -jar compiler.jar --js ../quolace.js --js_output_file quolace.min.debug.js --compilation_level ADVANCED_OPTIMIZATIONS --externs externs/jquery-1.7.js --formatting pretty_print
